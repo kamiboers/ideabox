8885bfcd788f117eaa63a46af73d8430b2c99cae
Kami Boers <kamiboers@gmail.com>
2016-06-30 02:49:34 -0600

    Tagging two (#4)
    * update gemfile to replace sqlite3 with postgresql
    * tag filtering functional, dropdown menu autopopulates on page load
    * ideas were not deleting because of foreign key relationship on idea_tag table, fixed this by deleting all idea_tags when deleting ideas

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,20 @@ Rails.application.routes.draw do
 
   root 'ideas#index'
 
-  get '/api/v1/upvote/:id', to: 'api/v1/ideas#upvote'
-  get '/api/v1/downvote/:id', to: 'api/v1/ideas#downvote'
-  get '/api/v1/ideas/:id', to: 'api/v1/ideas#update'
-  get '/api/v1/save/:id', to: 'api/v1/ideas#save_text'
-  get '/api/v1/tags/:id', to: 'api/v1/ideas#tags'
-
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :ideas, only: [:index, :create, :destroy]
     end
   end
+
+  get '/api/v1/upvote/:id', to: 'api/v1/ideas#upvote'
+  get '/api/v1/downvote/:id', to: 'api/v1/ideas#downvote'
+  get '/api/v1/ideas/:id', to: 'api/v1/ideas#destroy'
+  get '/api/v1/save/:id', to: 'api/v1/ideas#save_text'
+  get '/api/v1/tags/:id', to: 'api/v1/ideas#tags'
+  get '/api/v1/get_tags', to: 'api/v1/ideas#all_tags'
+  get '/api/v1/by_tag', to: 'api/v1/ideas#by_tag'
+
 
 
 end
